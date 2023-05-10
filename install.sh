95b703bb53d8c97e02b871d563848557f705b9c7
Ivan Rodkin <irodkin2002@gmail.com>
2023-05-10 20:57:36 +0300

    add requirements scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo python3.8 -m pip install -e .
+sudo python3.8 -m deeppavlov install nq_fid
+sudo python3.8 -m deeppavlov download en_odqa_bpr_croped_fid
+sudo python3.8 -m pip install protobuf==3.20.0 numpy==1.19.0
+
---
+++ prepare.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install --yes --force-yes python3.8-dev git python3-pip python3-venv
+sudo git clone https://github.com/studio-ousia/bpr.git
+cd /bpr
+sudo python3.8 -m pip install --upgrade build
+sudo python3.8 -m build
+sudo python3.8 -m pip install dist/bpr-0.0.1.tar.gz
+sudo python3.8 -m pip install dff[telegram]
+sudo python3.8 -m pip install -U coverage
