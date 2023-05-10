60182e2f9f5b5e9ffa7c07d15492128ed124ef90
Ivan Rodkin <irodkin2002@gmail.com>
2023-05-10 23:06:47 +0300

    ci fix

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,15 @@
 cd /
-sudo apt-get update
-sudo apt-get install --yes --force-yes git python3-pip python3-venv
-sudo git clone https://github.com/studio-ousia/bpr.git
+apt update
+apt install --yes --force-yes git python3-pip python3-venv
+git clone https://github.com/studio-ousia/bpr.git
 cd /bpr
-sudo python3 -m pip install --upgrade build
-sudo python3 -m build
-sudo python3 -m pip install dist/bpr-0.0.1.tar.gz
-sudo python3 -m pip install dff[telegram]
-sudo python3 -m pip install -U coverage
+echo "############################################"
+python3 --version
+echo "############################################"
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade build
+python3 -m build
+python3 -m pip install dist/bpr-0.0.1.tar.gz
+python3 -m pip install dff[telegram]
+python3 -m pip install -U coverage
 
