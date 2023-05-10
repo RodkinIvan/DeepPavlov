07049e1bb3a4ae2d8267b2b8dfa2253e03ddcd2b
Ivan Rodkin <irodkin2002@gmail.com>
2023-05-10 21:38:45 +0300

    fix ci

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo python3.8 -m pip install -e .
-sudo python3.8 -m deeppavlov install nq_fid
-sudo python3.8 -m deeppavlov download en_odqa_bpr_croped_fid
-sudo python3.8 -m pip install protobuf==3.20.0 numpy==1.19.0
+sudo python3 -m pip install -e .
+sudo python3 -m deeppavlov install nq_fid
+sudo python3 -m deeppavlov download en_odqa_bpr_croped_fid
+sudo python3 -m pip install protobuf==3.20.0 numpy==1.19.0
 
